574f7c8efc6be01448b90e7a40278f8561f3ba31
thiagonmiziara <thiagonmiziara@gmail.com>
2020-08-19 20:20:04 -0300

    add campos privados utilizando a convenção js com o caractere _

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 class cliente {
     nome;
     cpf;
+}
+
+class contaCorrente {
     agencia;
-    saldo;
+    //#saldo = o https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+        }
+    }
+
+    depositar(valor) {
+        if (valor > 0) {
+            this._saldo += valor;
+        }
+    }
 }
 
 const cliente1 = new cliente();
-const cliente2 = new cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-
+const cliente2 = new cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new contaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+contaCorrenteRicardo.sacar(50);
+
+
+console.log(contaCorrenteRicardo);
